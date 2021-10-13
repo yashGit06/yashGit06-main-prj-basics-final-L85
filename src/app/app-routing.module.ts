c032dfdc814e7b3b23a89baed888759b66ef47b5
yashGit06 <yashwantkumarsahu2014@gmail.com>
2021-10-14 01:02:04 +0530

    L-160 : Adding blank non functional Recipe + RecipeStartComponent - part 0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ const appRoute: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent,
         children : [
-            {path:'',component: RecipeStartComponent},
-            {path:':id',component: RecipesComponent}
+            {path:'',component: RecipeStartComponent}, 
+            {path:':id',component: RecipesComponent} 
         ]
     },
     { path: 'shopping-list', component: ShoppingListComponent }
