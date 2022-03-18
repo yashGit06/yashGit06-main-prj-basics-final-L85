27eba74eac8f9ca63041912569df4d241b2c1552
yashGit06 <yashwantkumarsahu2014@gmail.com>
2022-03-18 21:00:17 +0530

    L - 329 Core module concept

---
+++ src/app/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptorService } from "./auth/auth-interceptors";
+import { RecipeService } from "./recipes/recipe.service";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
+
+@NgModule({
+    providers: [
+        ShoppingListService,
+        RecipeService, 
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
+    ]
+})
+export class CoreModule { }
